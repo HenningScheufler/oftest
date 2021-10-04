ea56a78d20dfbd3203200b8f42bf6ef5611c17a3
Scheufler, Henning <henning.scheufler@dlr.de>
2021-10-04 11:38:55 +0200

    ENH: added function to easily load the expected results

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='oftest',
-    version='0.0.5',
+    version='0.1.0',
     license='GPLv3',
     description='test framework for OpenFOAM',
     long_description='%s\n%s' % (
--- tests/test_oftest_functions.py
+++ tests/test_oftest_functions.py
@@ -32,54 +32,7 @@ def test_run_time():
     assert t_exec == 30.55
     assert t_clock == 30.0
 
+def test_exp_results():
+    test = oftest.expected_results([1,2],('isoAlpha',32))
+    assert test['err_shape'] == 6.511E-02
 
-# def test_answer(request):
-#     print(request.config.getoption("--writeNow"))
-#     print(request.fixturenames)
-#     assert True
-
-
-# from datetime import datetime, timedelta
-
-# testdata = [
-#     (datetime(2001, 12, 12), datetime(2001, 12, 11), timedelta(1)),
-#     (datetime(2001, 12, 11), datetime(2001, 12, 12), timedelta(-1)),
-# ]
-
-# @pytest.mark.parametrize("a,b,expected", testdata)
-# def test_timedistance_v0(a, b, expected):
-#     diff = a - b
-#     assert diff == expected
-
-# @pytest.mark.parametrize("a,b,expected", testdata, ids=["forward", "backward"])
-# def test_timedistance_v1(a, b, expected):
-#     diff = a - b
-#     assert diff == expected
-
-
-# def idfn(val):
-#     if isinstance(val, (datetime,)):
-#         # note this wouldn't show any hours/minutes/seconds
-#         return val.strftime("%Y%m%d")
-
-
-# @pytest.mark.parametrize("a,b,expected", testdata, ids=idfn)
-# def test_timedistance_v2(a, b, expected):
-#     diff = a - b
-#     assert diff == expected
-
-
-# @pytest.mark.parametrize(
-#     "a,b,expected",
-#     [
-#         pytest.param(
-#             datetime(2001, 12, 12), datetime(2001, 12, 11), timedelta(1), id="forward"
-#         ),
-#         pytest.param(
-#             datetime(2001, 12, 11), datetime(2001, 12, 12), timedelta(-1), id="backward"
-#         ),
-#     ],
-# )
-# def test_timedistance_v3(a, b, expected):
-#     diff = a - b
-#     assert diff == expected
